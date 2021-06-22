5e5839d792790d7199e3c679365f3b48a2a4e1a5
Abi Baxter <AbiABaxter@gmail.com>
2021-06-22 02:36:14 +0100

    Enemies collide with walls

--- src/enemy.c
+++ src/enemy.c
@@ -17,6 +17,44 @@ DoEnemy(enemy * Enemy) {
         }
     }
     
+    Enemy->Knockback = V2Scale(Enemy->Knockback, (1 - 2 * Platform->Delta));
+    v2 Movement = v2(0, 0);
+    v2 Velocity = AddV2(Movement, Enemy->Knockback);
+    
+    v2 PotentialPosition = AddV2(Enemy->Position, Velocity);
+    v4 EnemyRect = v4(PotentialPosition.x - PLAYER_SIZE * 0.5,
+                      PotentialPosition.y - PLAYER_SIZE * 0.5,
+                      PLAYER_SIZE, PLAYER_SIZE);
+    
+    if(AttemptMove(EnemyRect)) {
+        Enemy->Position = PotentialPosition;
+        Enemy->Hitbox   = EnemyRect;
+    }
+    else {
+        if(Velocity.x) {
+            f32 PrevY = Velocity.y;
+            Velocity.y = 0;
+            v2 PotentialPosition = AddV2(Enemy->Position, Velocity);
+            v4 PlayerRect = v4(PotentialPosition.x - PLAYER_SIZE * 0.5,
+                               PotentialPosition.y - PLAYER_SIZE * 0.5,
+                               PLAYER_SIZE, PLAYER_SIZE);
+            if(AttemptMove(EnemyRect)) {
+                Enemy->Position = PotentialPosition;
+                Enemy->Hitbox   = EnemyRect;
+            }
+            else if(PrevY) {
+                Velocity.y = PrevY;
+                Velocity.x = 0;
+                v2 PotentialPosition = AddV2(Enemy->Position, Velocity);
+                v4 PlayerRect = v4(PotentialPosition.x - PLAYER_SIZE * 0.5,
+                                   PotentialPosition.y - PLAYER_SIZE * 0.5,
+                                   PLAYER_SIZE, PLAYER_SIZE);
+                if(AttemptMove(EnemyRect)) 
+                    Enemy->Position = PotentialPosition;
+            }
+        }
+    }
+    
     
     v4 Colour = v4(1.f, 1.f, 1.f, 1.f);
     if(Enemy->TimeSinceHit < 0.5) {
@@ -24,11 +62,6 @@ DoEnemy(enemy * Enemy) {
         Colour.z = 0.5 + Enemy->TimeSinceHit;
         Enemy->TimeSinceHit += Platform->Delta;
     }
-    
-    Enemy->Knockback = V2Scale(Enemy->Knockback, (1 - 2 * Platform->Delta));
-    v2 Movement = v2(0, 0);
-    v2 Velocity = AddV2(Movement, Enemy->Knockback);
-    Enemy->Position = AddV2(Enemy->Position, Velocity);
     
     DrawSprite(&Enemy->Sprite, Enemy->Position, Colour);
 }
